03578096d6a9ce363ee8448eab8bdb22d33d1855
Lucas Perretta <lucasperretta@gmail.com>
2018-07-22 23:39:07 -0300

    Initial Commit

---
+++ CocoaX/CocoaX.h
@@ -0,0 +1,19 @@
+//
+//  CocoaX.h
+//  CocoaX
+//
+//  Created by Lucas Perretta on 22/7/18.
+//  Copyright © 2018 Lucas Perretta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CocoaX.
+FOUNDATION_EXPORT double CocoaXVersionNumber;
+
+//! Project version string for CocoaX.
+FOUNDATION_EXPORT const unsigned char CocoaXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoaX/PublicHeader.h>
+
+
